796c887f7cdcca528f8118127eb739573a758899
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2023-10-09 00:12:48 +0300

    Changed appspec.yml

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 cd /home/ec2-user/server
 sudo java -jar -Dserver.port=80 \
-    DB_lab5-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+    *.jar > /dev/null 2> /dev/null < /dev/null &
