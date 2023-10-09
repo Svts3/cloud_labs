ec8abce436399494591ee6bb2442ee42673353a5
Svyatoslav Tsar <svyatko.tsar@gmail.com>
2023-10-09 23:42:59 +0300

    added test endpoint

--- src/main/java/ua/lviv/iot/controller/AuthController.java
+++ src/main/java/ua/lviv/iot/controller/AuthController.java
@@ -24,6 +24,11 @@ public class AuthController {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+
     @PostMapping("/register")
     public ResponseEntity<String>register(@RequestBody OwnerDTO ownerDTO) throws Exception {
         if(ownerService.existsByEmail(ownerDTO.getEmail())){
